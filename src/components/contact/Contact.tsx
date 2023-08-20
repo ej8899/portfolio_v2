b38411bf2a8dcb70ec9f1d49732d6e35c8dc68f5
ej8899 <ej8899@gmail.com>
2023-08-20 15:40:00 -0400

    fix: form response SVGs fix to proper positions

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -71,7 +71,7 @@ function Contact() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          access_key: '7160e73c-4a32-4952-ab02-e07ea131ed58',
+          access_key: '7160e73c-4a32-4952-ab02-e07ea131ed58-asdfds',
           from_name: 'erniejohnson.ca',
           subject: 'erniejohnson.ca - contact form response',
           message,
@@ -204,70 +204,90 @@ function Contact() {
 
           {/* animated hourglass */}
           {isSendPending && (
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='send-status pending'
-              viewBox='0 0 24 24'
-            >
-              <path
-                fill='var(--clr-neutral-500)'
-                d='M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z'
-                opacity='.5'
-              />
-              <path
-                fill='currentColor'
-                d='M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z'
-                opacity='.75'
-              >
-                <animateTransform
-                  attributeName='transform'
-                  dur='2s'
-                  from='0 12 12'
-                  repeatCount='indefinite'
-                  to='360 12 12'
-                  type='rotate'
-                />
-              </path>
-            </svg>
+            <div className='centered_container'>
+              <div className='centered_item'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='send-status pending'
+                  viewBox='0 0 24 24'
+                >
+                  <path
+                    fill='var(--clr-neutral-500)'
+                    d='M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z'
+                    opacity='.5'
+                  />
+                  <path
+                    fill='currentColor'
+                    d='M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z'
+                    opacity='.75'
+                  >
+                    <animateTransform
+                      attributeName='transform'
+                      dur='2s'
+                      from='0 12 12'
+                      repeatCount='indefinite'
+                      to='360 12 12'
+                      type='rotate'
+                    />
+                  </path>
+                </svg>
+              </div>
+            </div>
           )}
 
           {/* green checkmark icon */}
           {isSent && (
             <>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='send-status sent'
-                viewBox='0 0 24 24'
-              >
-                <path
-                  fill='var(--clr-success)'
-                  d='m10.6 16.6l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4l4.25 4.25ZM12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z'
-                />
-              </svg>
-              <p className='thanksmessage'>
-                Thank you for reaching out...
-                <br />I will respond to your message asap!
-              </p>
+              <div className='centered_container'>
+                <div className='centered_item'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='send-status sent'
+                    viewBox='0 0 24 24'
+                  >
+                    <path
+                      fill='var(--clr-success)'
+                      d='m10.6 16.6l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4l4.25 4.25ZM12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z'
+                    />
+                  </svg>
+                  <p className='thanksmessage'>
+                    Thank you for reaching out...
+                    <br />I will respond to your message asap!
+                  </p>
+                </div>
+              </div>
+            </>
+          )}
+
+          {/* error during send */}
+          {isError && (
+            <>
+              <div className='centered_container'>
+                <div className='centered_item'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='send-status error'
+                    viewBox='0 0 24 24'
+                  >
+                    <path
+                      fill='var(--clr-error)'
+                      d='M12 17q.425 0 .713-.288T13 16q0-.425-.288-.713T12 15q-.425 0-.713.288T11 16q0 .425.288.713T12 17Zm0-4q.425 0 .713-.288T13 12V8q0-.425-.288-.713T12 7q-.425 0-.713.288T11 8v4q0 .425.288.713T12 13Zm0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z'
+                    />
+                  </svg>
+                  <p>
+                    Something went wrong. <br />
+                    <br />
+                    Email me directly at{' '}
+                    <a href='mailto:ernie@erniejohnson.ca'>
+                      <b>ernie@erniejohnson.ca</b>
+                    </a>
+                  </p>
+                </div>
+              </div>
             </>
           )}
         </div>
       </div>
-
-      {/* error during send */}
-      {isError && (
-        <>
-          <svg xmlns='http://www.w3.org/2000/svg' className='send-status error' viewBox='0 0 24 24'>
-            <path
-              fill='var(--clr-error)'
-              d='M12 17q.425 0 .713-.288T13 16q0-.425-.288-.713T12 15q-.425 0-.713.288T11 16q0 .425.288.713T12 17Zm0-4q.425 0 .713-.288T13 12V8q0-.425-.288-.713T12 7q-.425 0-.713.288T11 8v4q0 .425.288.713T12 13Zm0 9q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z'
-            />
-          </svg>
-          <p>
-            Something went wrong. Email me directly at{' '}
-            <a href='mailto:ernie@erniejohnson.ca'>ernie@erniejohnson.ca</a>
-          </p>
-        </>
-      )}
     </section>
   );
 }
