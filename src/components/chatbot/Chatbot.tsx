f2f16ef1d5e5a73cd17788120b0fa9d2cea64118
ej8899 <ej8899@gmail.com>
2023-08-22 10:31:24 -0400

    update: chatbot style tweaks, add more responses

--- src/components/chatbot/Chatbot.tsx
+++ src/components/chatbot/Chatbot.tsx
@@ -19,6 +19,10 @@ type TProps = {
 
 // TODO need to ensure we check for full words - presently 'history' triggers based on 'hi' in the greetings!
 const intents = {
+  commands: {
+    patterns: ['command','commands',],
+    responses: ['Sure thing!<br><br>Here are a few common commands:<br>quit, support, feedback, about chatterbot, developer skills.']
+  },
   greetings: {
     patterns: ['hello', 'hi', 'hey','wassup','sup','greetings'],
     responses: ['Hello!', 'Hi there!', 'Hey! How can I assist you?']
@@ -28,7 +32,7 @@ const intents = {
     responses: ['Sure, I\'m here to help!', 'What do you need assistance with?', 'I\'m here to assist you.']
   },
   bye: {
-    patterns: ['bye', 'goodbye', 'exit','quit','logout'],
+    patterns: ['bye', 'goodbye', 'exit','quit','logout','reset'],
     responses: ['Goodbye!', 'Have a great day!', 'See you later!']
   },
   contact: {
@@ -44,20 +48,32 @@ const intents = {
     responses: ['Ernie has experience in Javascript, Typescript, React, NextJS, C, C++, PHP, Perl and more. Is there something more specific you need?']
   },
   hackAttempt: {
-    patterns: ['<script>', '<link>', '<iframe>','${',],
+    patterns: ['script', 'link', 'iframe','${','hack','backdoor','password',],
     responses: ['Ernie has experience in cybersecurity for applications. This attack has been blocked!']
   },
   aboutBot: {
     patterns: ['about', 'chatterbot',],
-    responses: ['Chatterbot is in it&amp;s infancy, but built as a React component to work in this project. As an extra "did you know", Chatterbot was built largely by an AI system with a few prompts from Ernie, followed up with some of his human tweaks of course!']
+    responses: ['Chatterbot is in it`s infancy, but built as a React component to work within this project. As an extra "did you know", Chatterbot was built largely by an AI system with a few prompts from Ernie, followed up with some of his human tweaks of course!']
+  },
+  botChangeLog: {
+    patterns: ['changelog', 'change log'],
+    responses: ['Here is the changelog for Chatterbot:<br>2023-08 - 1.0 - initial release'],
   },
   downloads: {
     patterns: ['resume'],
     responses: ['Ernie&#39;s resume can be downloaded <a href="http://www.erniejohnson.ca/resume.pdf">here</a>'],
   },
+  botName: {
+    patterns: ['your name','bot name'],
+    responses: ['My name is Chatterbot 1.0'],
+  },
   usersName: {
     patterns: ['name'],
     responses: ['Your name is {userName}']
+  },
+  saveMessages: {
+    patterns: ['save','message history','history'],
+    responses: ['Appologies,<br>but this is something I can not do quite yet.']
   },
   default: {
     responses: ["I'm sorry, I don't understand that.", "Could you please rephrase that?", "I'm not sure how to help with that."]
@@ -69,8 +85,14 @@ function getResponse(message) {
 
   for (const intentName in intents) {
     const intent = intents[intentName];
+    // for (const pattern of intent.patterns || []) {
+    //   if (lowerMessage.includes(pattern)) {
+    //     return getRandomResponse(intent.responses);
+    //   }
+    // }
     for (const pattern of intent.patterns || []) {
-      if (lowerMessage.includes(pattern)) {
+      const regexPattern = new RegExp(`\\b${pattern}\\b`, 'i'); // Create regex with word boundaries
+      if (regexPattern.test(lowerMessage)) {
         return getRandomResponse(intent.responses);
       }
     }
@@ -84,6 +106,7 @@ function getRandomResponse(responses) {
 }
 
 
+
 //
 // main Chatbot() system
 //
@@ -92,6 +115,7 @@ function Chatbot() {
   const [userInput, setUserInput] = useState('');
   const [messageHistory, setMessageHistory] = useState([]);
   const [userName, setUserName] = useState('');
+  const messagesEndRef = useRef(null);
 
   const toggleChatbot = () => {
     setIsOpen(!isOpen);
@@ -99,6 +123,12 @@ function Chatbot() {
 
   const handleUserInput = (event) => {
     setUserInput(event.target.value);
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSendMessage();
+    }
   };
 
   const handleSendMessage = () => {
@@ -115,6 +145,11 @@ function Chatbot() {
         const botResponse = getResponse(userInput);
         addMessage('Bot', botResponse);
       }
+      // Scroll to the bottom of the chat history
+      setTimeout(() => {
+        messagesEndRef.current.scrollTop = messagesEndRef.current.scrollHeight;
+      }, 0);
+      
       setUserInput('');
     }
   };
@@ -126,7 +161,6 @@ function Chatbot() {
     ]);
   };
   
-  const messagesEndRef = useRef(null);
 
   useEffect(() => {
     if (messagesEndRef.current) {
@@ -135,7 +169,7 @@ function Chatbot() {
   }, [messageHistory]);
   
   useEffect(() => {
-    addMessage('Bot', `ue What is your name?`);
+    if(!userName) addMessage('Bot', `ue What is your name?`);
   }, []);
 
   return (
@@ -155,7 +189,7 @@ function Chatbot() {
               </div>
             )}
             <div className="message-content">
-              {msg.message.includes('<a href=') ? (
+              {msg.message.includes('<a href=') || msg.message.includes('<b>') || msg.message.includes('<br>') ? (
                   <div dangerouslySetInnerHTML={{ __html: msg.message }} />
                 ) : msg.message.includes('{userName}') ? (
                   msg.message.replace('{userName}', userName)
@@ -173,8 +207,9 @@ function Chatbot() {
           value={userInput}
           onChange={handleUserInput}
           placeholder="Type your message..."
+          onKeyDown={handleKeyDown}
         />
-        <button onClick={handleSendMessage}>Send</button>
+        &nbsp;&nbsp;&nbsp;<button onClick={handleSendMessage}>Send</button>
       </div>
     </div>
   </div>
