34574289567539136bdade42be9a3aef3a1a9d6c
ej8899 <ej8899@gmail.com>
2023-08-23 11:47:45 -0400

    update: fix scoring system for accuracy

--- src/components/chatbot/Chatbot.tsx
+++ src/components/chatbot/Chatbot.tsx
@@ -25,83 +25,125 @@ const intents = {
     patterns: ['options'],
     responses: ['common options to use:'],
     quickReplies: ['commands','contact','about','soft skills'],
+    replies: 0,
   },
   donate: {
     patterns: ['donate','donations','buy me a coffee', 'buy coffee', 'tips'],
-    responses: ['That is great that you want to buy the developer a coffee! <a href="https://www.buymeacoffee.com/ej88994">click here</a> to "buy me a coffee"']
+    responses: ['That is great that you want to buy the developer a coffee! <a href="https://www.buymeacoffee.com/ej88994">click here</a> to "buy me a coffee"'],
+    replies: 0,
   },
   commands: {
     patterns: ['command','commands',],
-    responses: ['Sure thing!<br><br>Here are a few common commands:<br>quit, support, feedback, about chatterbot, developer skills.']
+    responses: ['Sure thing!<br><br>Here are a few common commands:<br>quit, support, feedback, about chatterbot, developer skills.'],
+    replies: 0,
   },
   greetings: {
     patterns: ['hello', 'hi', 'hey','wassup','sup','greetings'],
-    responses: ['Hello!', 'Hi there!', 'Hey! How can I assist you?']
+    responses: ['Hello!', 'Hi there!', 'Hey! How can I assist you?'],
+    replies: 0,
   },
   help: {
     patterns: ['help', 'support', 'assistance'],
-    responses: ['Sure, I\'m here to help!', 'What do you need assistance with?', 'I\'m here to assist you.']
+    responses: ['Sure, I\'m here to help!', 'What do you need assistance with?', 'I\'m here to assist you.'],
+    replies: 0,
   },
   bye: {
     patterns: ['bye', 'goodbye', 'exit','quit','logout','reset','close'],
-    responses: ['Goodbye!', 'Have a great day!', 'See you later!']
+    responses: ['Goodbye!', 'Have a great day!', 'See you later!'],
+    replies: 0,
   },
   contact: {
     patterns: ['contact', 'phone', 'email','operator','support','live'],
-    responses: ['You can contact Ernie through the <a href="#contact">contact</a> section of the site, or email ernie@erniejohnson.ca']
+    responses: ['You can contact Ernie through the <a href="#contact">contact</a> section of the site, or email ernie@erniejohnson.ca'],
+    replies: 0,
   },
   feedback: {
     patterns: ['feedback','comments','comment'],
-    responses: ['Your feedback will be valuable! Share your thoughts and suggestions in the "<a href="#contact">contact</a>" section!']
+    responses: ['Your feedback will be valuable! Share your thoughts and suggestions in the "<a href="#contact">contact</a>" section!'],
+    replies: 0,
   },
   softSkills: {
     patterns: ['softskills','soft skills','project management'],
     responses: ['Ernie has, of course, numerous "soft skills" he has gained during many years of employment across several different industries. The ability to work independently and within a team, communications, project management and more.'],
+    replies: 0,
   },
   codeSkills: {
     patterns: ['coding skills', 'skills', 'frameworks', 'libraries','languages','json','javascript','react','typescript','nextjs','c','perl','php','bootstrap','materialui','tailwind'],
-    responses: ['Ernie has experience in Javascript, Typescript, React, NextJS, C, C++, and more. He is also skilled with backend technologies like NodeJS, Perl, PHP, MySQL, PostgreSQL and more.<br><br>You can find additional skills in the <a href="#about">about</a> section.<BR>Is there something more specific you need?']
+    responses: ['Ernie has experience in Javascript, Typescript, React, NextJS, C, C++, and more. He is also skilled with backend technologies like NodeJS, Perl, PHP, MySQL, PostgreSQL and more.<br><br>You can find additional skills in the <a href="#about">about</a> section.<BR>Is there something more specific you need?'],
+    replies: 0,
   },
   workOptions: {
     patterns: ['work', 'freelance', 'availability'],
-    responses: ['Ernie is currently available for freelance, part-time and full-time work. ']
+    responses: ['Ernie is currently available for freelance, part-time and full-time work. '],
+    replies: 0,
   },
   hackAttempt: {
     patterns: ['script', 'link', 'iframe','${','hack','backdoor','password','cybersecurity','security'],
-    responses: ['Ernie has experience in cybersecurity for applications and tends not to leave security vulnerabilities open for possible hack attempts.']
+    responses: ['Ernie has experience in cybersecurity for applications and tends not to leave security vulnerabilities open for possible hack attempts.'],
+    replies: 0,
   },
   aboutBot: {
     patterns: ['about', 'chatterbot','who are you'],
-    responses: ['I am Chatterbot.<br><br>Although in my infancy, I am built as a React component to work within this project<br><br>As an extra "did you know", Chatterbot was built largely by an AI system with a few prompts from Ernie, followed up with some of his human tweaks of course!']
+    responses: ['I am Chatterbot.<br><br>Although in my infancy, I am built as a React component to work within this project<br><br>As an extra "did you know", Chatterbot was built largely by an AI system with a few prompts from Ernie, followed up with some of his human tweaks of course!'],
+    replies: 0,
   },
   botChangeLog: {
     patterns: ['changelog', 'change log', 'version'],
     responses: ['Here is the recent change log for Chatterbot:<br>' + chatbotChangeLog],
+    replies: 0,
   },
   downloads: {
     patterns: ['resume','download','downloads'],
     responses: ['Ernie&#39;s resume can be downloaded <a href="http://www.erniejohnson.ca/resume.pdf">here</a>'],
+    replies: 0,
   },
   useAI: {
     patterns: ['ai','bard','chatgpt','chat gpt'],
     responses: ['Yes, Ernie uses AI technologies like ChatGPT and Google Bard.<br><br>In fact, Chatterbot was written largely by prompting ChatGPT to create the outline and basics of functionality.  Sure, there were a few bugs that needed a human to iron out along the way, and there may not have been much of a time saver from writing it from scratch, but interesting just the same!<br><br>That being said, Chatterbot responses are not generated by AI.'],
+    replies: 0,
   },
   botName: {
     patterns: ['your name','bot name'],
     responses: ['My name is Chatterbot v' + chatbotVersion],
+    replies: 0,
   },
   usersName: {
     patterns: ['name'],
-    responses: ['Your name is {userName}']
+    responses: ['Your name is {userName}'],
+    replies: 0,
   },
   saveMessages: {
     patterns: ['save','message history','history'],
-    responses: ['Appologies,<br>but this is something I can not do quite yet.']
+    responses: ['Appologies,<br>but this is something I can not do quite yet.'],
+    replies: 0,
   },
   default: {
-    responses: ["I'm sorry, I don't understand that.", "Could you please rephrase that?", "I'm not sure how to help with that."]
+    responses: ["I'm sorry, I don't understand that.", "Could you please rephrase that?", "I'm not sure how to help with that."],
+    replies: 0,
   }
 };
+
+let unMatchedInputs = [];
+
+const chatbotScoreMax = Object.keys(intents).length - 1;
+
+// chatbot scoring calculator
+function sumPositiveReplies(obj) {
+  let sum = 0;
+
+  for (const key in obj) {
+    if (typeof obj[key] === "object" && obj[key] !== null) {
+      if ("replies" in obj[key] && typeof obj[key].replies === "number" && obj[key].replies > 0) {
+        sum += 1;
+      }
+    }
+  }
+  // don't count default replies (remove if added)
+  if(obj.default.replies > 0) {
+    sum --;
+  }
+  return sum;
+}
 
 // HELPER FUNCTIONS
 function getTimePeriod() {
@@ -124,7 +166,11 @@ function getResponse(message) {
     for (const pattern of intent.patterns || []) {
       const regexPattern = new RegExp(`\\b${pattern}\\b`, 'i'); // Create regex with word boundaries
       if (regexPattern.test(lowerMessage)) {
-        chatbotScore++;
+        // chatbotScore++;
+        intent.replies++;
+        chatbotScore = sumPositiveReplies(intents);
+        console.log('intent:',intent,' replies:',intent.replies);
+        // console.log('# of intents:',Object.keys(intents).length);
         if(intentName === 'options') { 
           showQuickReplies = true; 
         } else {
@@ -135,6 +181,9 @@ function getResponse(message) {
     }
   }
   showQuickReplies = false;
+  intents.default.replies++;
+  unMatchedInputs.push(message);
+  console.log('no matches: ',unMatchedInputs);
   return getRandomResponse(intents.default.responses);
 }
 
@@ -247,7 +296,7 @@ function Chatbot() {
       <div className="chatbot-content"> 
       <div className="chatbot-header">
         <div className="title">ChatterBot</div>
-        <div className="score">Score: {score}</div>
+        <div className="score">Score: {score}/{chatbotScoreMax}</div>
         <button className="close-button" onClick={toggleChatbot}>Close</button>
       </div>
       <div className="message-history">
