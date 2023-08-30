d57674d7fcc92c0e60a29316b8ec27d6c85b045d
ej8899 <ej8899@gmail.com>
2023-08-30 00:07:23 -0400

    update: send icon instead of text

--- src/components/chatbot/Chatbot.tsx
+++ src/components/chatbot/Chatbot.tsx
@@ -13,7 +13,7 @@ import { ReactNode, useState, useRef, useEffect } from 'react';
 import './Chatbot.scss';
 import globalconfig from '../../config';
 import popOut from './newWindow';
-
+import SendIcon from './sendIcon';
 const chatbotVersion = '1.0';
 const chatbotChangeLog = 'v1.0 - 2023-08 - initial roll out';
 let showQuickReplies = false;
@@ -41,6 +41,8 @@ const calculateTimeDifference = () => {
 const popOutIcon =`<svg class="popoutIcon" xmlns='http://www.w3.org/2000/svg' height='0.7em' width='0.7em' viewBox='0 0 512 512'>
 <path d='M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z' />
 </svg>`;
+
+
 
 //
 // EASTER EGG Stuff.
@@ -207,6 +209,11 @@ const intents = {
       response: 'about chatterbot'
       },
     ],
+    replies: 0,
+  },
+  zzz: {
+    patterns: ['zzz'],
+    responses: [`Asleep at the keyboard are you?`],
     replies: 0,
   },
   greetingsDay: {
@@ -749,6 +756,7 @@ function Chatbot(props) {
   };
 
   const handleQuickLinks = (responseToParse) => {
+    if (!responseToParse) return;
     return(responseToParse.replace(/\[(.*?)\]/g, `<span class="keyword-link" data-keyword="$1">$1</span>`));
   };
 
@@ -760,9 +768,15 @@ function Chatbot(props) {
     // buttonRef.current.click();
   };
   
+  // BUG - blank message on send crashes chatterbot
   const handleSendMessage = (inputIntent) => {
     // setIsTyping(true);
     // TODO why is setState not functioning as expected  - because of async flow -- but how can we solve the problem better than this way?
+
+    if (userInput.trim() == '' && inputIntent == '') {
+      inputIntent = "zzz";
+      alert("blank");
+    }
 
     if (inputIntent) {
       addMessage('User', inputIntent);
@@ -795,7 +809,7 @@ function Chatbot(props) {
       }, 0);
       // setUserName('');
       setUserInput('');
-    }
+    } 
   };
 
   const addMessage = (sender, message) => {
@@ -982,7 +996,7 @@ function Chatbot(props) {
           placeholder="Type your message..."
           onKeyDown={handleKeyDown}
         />
-        &nbsp;&nbsp;&nbsp;<button ref={buttonRef} onClick={handleSendMessage}>Send</button>
+        &nbsp;&nbsp;&nbsp;<button ref={buttonRef} onClick={handleSendMessage}><SendIcon/></button>
       </div>
     </div>
   </div>
