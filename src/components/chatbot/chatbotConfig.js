aced7d96442870e6d3b9e00cfaf2847c71494a91
ej <ej8899@gmail.com>
2023-08-31 11:12:18 -0600

    add: chatbot separation of data from program

--- src/components/chatbot/chatbotConfig.js
+++ src/components/chatbot/chatbotConfig.js
@@ -7,9 +7,15 @@ export const configData = {
   brandingTitle: 'ChatterBot',
   version: '1.0',
   
+  allowScoring: true,
+  // TODO - build below feature
+  // this shows a 'typing' animation and slight delay before user gets response back
+  instantResponse: true,
+
   // TODO - add this feature
   allowBadLanguage: true,
 
+  // TODO - add support for these toggles
   easterEggs: true,
   allowLocation: true,
   allowJokes: true,
