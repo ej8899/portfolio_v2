55445f3be7ef0e23c5d92852eaaab35bbe983adc
ej <ej8899@gmail.com>
2023-08-31 10:33:39 -0600

    add: chatbot separation of data from program

---
+++ src/components/chatbot/chatbotConfig.js
@@ -0,0 +1,17 @@
+//
+// configData - for chatterbot
+//
+
+export const configData = {
+  debugMode: false,
+  brandingTitle: 'ChatterBot',
+  version: '1.0',
+  
+  // TODO - add this feature
+  allowBadLanguage: true,
+
+  easterEggs: true,
+  allowLocation: true,
+  allowJokes: true,
+
+};
