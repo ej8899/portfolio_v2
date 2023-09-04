afd51c616f951c46b4563252a55823fd3d2da2bc
ej <ej8899@gmail.com>
2023-09-03 18:41:46 -0600

    update: dummy project data for view testing

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -84,6 +84,52 @@ const PROJECTS: IProject[] = [
     ],
     bulletPoints: ['react-router-dom', 'react-icons', 'react-router-dom', 'vite'],
   },
+
+  {
+    title: 'filler app',
+    featured: false,
+    links: {
+      live: 'https://github.com',
+      repo: 'https://github.com',
+    },
+    techIcons: ['react', 'sass', 'postgres', 'express', 'node'],
+    screenshots: [
+      { url: learnthisHome, alt: 'The Comedy DB Desktop Home Screenshot' },
+      { url: learnthisLessons, alt: 'The Comedy DB Desktop Special Screenshot' },
+      { url: learnthisClipper, alt: 'The Comedy DB Desktop Special Screenshot' },
+      { url: learnthisAbout, alt: 'The Comedy DB Desktop Special Screenshot' },
+    ],
+    description: [
+      { type: 'header', value: 'The Inspiration' },
+      {
+        type: 'p',
+        value:
+          '1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      },
+      {
+        type: 'p',
+        value:
+          '2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      },
+      { type: 'header', value: 'High Level Breakdown' },
+      {
+        type: 'p',
+        value:
+          '3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ',
+      },
+      {
+        type: 'p',
+        value:
+          '4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..',
+      },
+      {
+        type: 'p',
+        value:
+          '5RLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      },
+    ],
+    bulletPoints: ['react-router-dom', 'react-icons', 'react-router-dom', 'vite'],
+  },
 ];
 
 export default PROJECTS;
