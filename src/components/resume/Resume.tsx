98f0c39f96ad906449aae5b3d45b399d76384c10
ej8899 <ej8899@gmail.com>
2023-09-07 19:03:43 -0400

    update: add top & bottom fades to timeline bar

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -41,175 +41,157 @@ function Resume() {
 
   const progressBarStyle = {
     height: `${scrollPercentage}%`, // Set the progress bar height based on scroll percentage
-    // backgroundColor: scrollPercentage < 50 ? 'blue' : 'green', // Change color at 50% scroll
   };
 
   return (
     <section id='resume' className='resume' aria-labelledby='contact__title' ref={sectionRef}>
       {/* section content */}
       <div className='column centered_grid'>
-        <div className='scroll-section'>
-          {/* Your section content goes here */}
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac augue eu ex viverra
-          hendrerit. Nulla facilisi. Phasellus vel tortor nec dui efficitur dignissim. Proin et
-          libero et quam tincidunt iaculis vel vel justo. Vivamus at mi non sapien posuere euismod
-          eget a nisi. Integer ut neque vel tortor luctus blandit. Fusce euismod, dui in vestibulum
-          facilisis, tellus tortor convallis nunc, a egestas ipsum nunc ut libero. Nulla facilisi.
-          Etiam non erat eu tellus vulputate ultrices eu non urna. Nam ut dolor nec ex vestibulum
-          accumsan in eu urna. Aenean sit amet facilisis urna. Sed id neque nec elit hendrerit
-          varius. Quisque a nunc libero. Nullam a ultrices arcu. Vivamus eu pharetra nisl. Praesent
-          posuere erat nec massa ultricies, vel ultrices justo tempus. In condimentum in libero at
-          congue. Vivamus vel diam lectus. Nunc euismod ullamcorper ex, id facilisis odio varius in.
-          Maecenas tristique nunc quis tristique gravida.
-          <div className='progress-bar-empty'></div>
-          <div className='progress-bar' style={progressBarStyle}></div>
+        <section className='timeline'>
           <div className='overlay-fade-top'></div>
+          <div className='timeline-container'>
+            <div className='timeline-item'>
+              <div className='timeline-img'></div>
+
+              <div className='timeline-content js--fadeInLeft'>
+                <h2>Title</h2>
+                <div className='date'>1 MAY 2016</div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione
+                  alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
+                  nostrum excepturi amet in dolores. Alias, ullam.
+                </p>
+                <button className='bnt-more'>More</button>
+              </div>
+            </div>
+
+            <div className='timeline-item'>
+              <div className='timeline-img'></div>
+
+              <div className='timeline-content timeline-card js--fadeInRight'>
+                <div className='timeline-img-header'>
+                  <h2>Card Title</h2>
+                </div>
+                <div className='date'>25 MAY 2016</div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione
+                  alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
+                  nostrum excepturi amet in dolores. Alias, ullam.
+                </p>
+                <button className='bnt-more'>More</button>
+              </div>
+            </div>
+
+            <div className='timeline-item'>
+              <div className='timeline-img'></div>
+
+              <div className='timeline-content js--fadeInLeft'>
+                <div className='date'>3 JUN 2016</div>
+                <h2>Quote</h2>
+                <blockquote>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta explicabo debitis
+                  omnis dolor iste fugit totam quasi inventore!
+                </blockquote>
+              </div>
+            </div>
+
+            <div className='timeline-item'>
+              <div className='timeline-img'></div>
+
+              <div className='timeline-content js--fadeInRight'>
+                <h2>Title</h2>
+                <div className='date'>22 JUN 2016</div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione
+                  omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae
+                  nostrum excepturi amet in dolores. Alias, ullam.
+                </p>
+                <button className='bnt-more'>More</button>
+              </div>
+            </div>
+
+            <div className='timeline-item'>
+              <div className='timeline-img'></div>
+
+              <div className='timeline-content timeline-card js--fadeInLeft'>
+                <div className='timeline-img-header'>
+                  <h2>Card Title</h2>
+                </div>
+                <div className='date'>10 JULY 2016</div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione
+                  alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
+                  nostrum excepturi amet in dolores. Alias, ullam.
+                </p>
+                <button className='bnt-more'>More</button>
+              </div>
+            </div>
+
+            <div className='timeline-item'>
+              <div className='timeline-img'></div>
+
+              <div className='timeline-content timeline-card js--fadeInRight'>
+                <div className='timeline-img-header'>
+                  <h2>Card Title</h2>
+                </div>
+                <div className='date'>30 JULY 2016</div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione
+                  alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
+                  nostrum excepturi amet in dolores. Alias, ullam.
+                </p>
+                <button className='bnt-more'>More</button>
+              </div>
+            </div>
+
+            <div className='timeline-item'>
+              <div className='timeline-img'></div>
+
+              <div className='timeline-content js--fadeInLeft'>
+                <div className='date'>5 AUG 2016</div>
+                <h2>Quote</h2>
+                <blockquote>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta explicabo debitis
+                  omnis dolor iste fugit totam quasi inventore!
+                </blockquote>
+              </div>
+            </div>
+
+            <div className='timeline-item'>
+              <div className='timeline-img'></div>
+
+              <div className='timeline-content timeline-card js--fadeInRight'>
+                <div className='timeline-img-header'>
+                  <h2>Card Title</h2>
+                </div>
+                <div className='date'>19 AUG 2016</div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione
+                  alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
+                  nostrum excepturi amet in dolores. Alias, ullam.
+                </p>
+                <button className='bnt-more'>More</button>
+              </div>
+            </div>
+
+            <div className='timeline-item'>
+              <div className='timeline-img'></div>
+
+              <div className='timeline-content js--fadeInLeft'>
+                <div className='date'>1 SEP 2016</div>
+                <h2>Title</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione
+                  alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
+                  nostrum excepturi amet in dolores. Alias, ullam.
+                </p>
+                <button className='bnt-more'>More</button>
+              </div>
+            </div>
+          </div>
           <div className='overlay-fade-bottom'></div>
-        </div>
+        </section>
       </div>
-
-      <section className='timeline'>
-        <div className='container'>
-          <div className='timeline-item'>
-            <div className='timeline-img'></div>
-
-            <div className='timeline-content js--fadeInLeft'>
-              <h2>Title</h2>
-              <div className='date'>1 MAY 2016</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis
-                alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
-                nostrum excepturi amet in dolores. Alias, ullam.
-              </p>
-              <button className='bnt-more'>More</button>
-            </div>
-          </div>
-
-          <div className='timeline-item'>
-            <div className='timeline-img'></div>
-
-            <div className='timeline-content timeline-card js--fadeInRight'>
-              <div className='timeline-img-header'>
-                <h2>Card Title</h2>
-              </div>
-              <div className='date'>25 MAY 2016</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis
-                alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
-                nostrum excepturi amet in dolores. Alias, ullam.
-              </p>
-              <button className='bnt-more'>More</button>
-            </div>
-          </div>
-
-          <div className='timeline-item'>
-            <div className='timeline-img'></div>
-
-            <div className='timeline-content js--fadeInLeft'>
-              <div className='date'>3 JUN 2016</div>
-              <h2>Quote</h2>
-              <blockquote>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta explicabo debitis
-                omnis dolor iste fugit totam quasi inventore!
-              </blockquote>
-            </div>
-          </div>
-
-          <div className='timeline-item'>
-            <div className='timeline-img'></div>
-
-            <div className='timeline-content js--fadeInRight'>
-              <h2>Title</h2>
-              <div className='date'>22 JUN 2016</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis
-                alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
-                nostrum excepturi amet in dolores. Alias, ullam.
-              </p>
-              <button className='bnt-more'>More</button>
-            </div>
-          </div>
-
-          <div className='timeline-item'>
-            <div className='timeline-img'></div>
-
-            <div className='timeline-content timeline-card js--fadeInLeft'>
-              <div className='timeline-img-header'>
-                <h2>Card Title</h2>
-              </div>
-              <div className='date'>10 JULY 2016</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis
-                alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
-                nostrum excepturi amet in dolores. Alias, ullam.
-              </p>
-              <button className='bnt-more'>More</button>
-            </div>
-          </div>
-
-          <div className='timeline-item'>
-            <div className='timeline-img'></div>
-
-            <div className='timeline-content timeline-card js--fadeInRight'>
-              <div className='timeline-img-header'>
-                <h2>Card Title</h2>
-              </div>
-              <div className='date'>30 JULY 2016</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis
-                alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
-                nostrum excepturi amet in dolores. Alias, ullam.
-              </p>
-              <button className='bnt-more'>More</button>
-            </div>
-          </div>
-
-          <div className='timeline-item'>
-            <div className='timeline-img'></div>
-
-            <div className='timeline-content js--fadeInLeft'>
-              <div className='date'>5 AUG 2016</div>
-              <h2>Quote</h2>
-              <blockquote>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta explicabo debitis
-                omnis dolor iste fugit totam quasi inventore!
-              </blockquote>
-            </div>
-          </div>
-
-          <div className='timeline-item'>
-            <div className='timeline-img'></div>
-
-            <div className='timeline-content timeline-card js--fadeInRight'>
-              <div className='timeline-img-header'>
-                <h2>Card Title</h2>
-              </div>
-              <div className='date'>19 AUG 2016</div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis
-                alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
-                nostrum excepturi amet in dolores. Alias, ullam.
-              </p>
-              <button className='bnt-more'>More</button>
-            </div>
-          </div>
-
-          <div className='timeline-item'>
-            <div className='timeline-img'></div>
-
-            <div className='timeline-content js--fadeInLeft'>
-              <div className='date'>1 SEP 2016</div>
-              <h2>Title</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis
-                alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor,
-                nostrum excepturi amet in dolores. Alias, ullam.
-              </p>
-              <button className='bnt-more'>More</button>
-            </div>
-          </div>
-        </div>
-      </section>
     </section>
   );
 }
