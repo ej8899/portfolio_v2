f9434aa54a95f5bb59c02889a6e35ebe507758f8
ej8899 <ej8899@gmail.com>
2023-09-08 06:13:40 -0400

    update: tweak divider colors

--- src/components/resume/Resume.tsx
+++ src/components/resume/Resume.tsx
@@ -84,7 +84,7 @@ function Resume() {
           viewBox='0 0 1200 120'
           preserveAspectRatio='none'
         >
-          <path d='M1200 120L0 16.48 0 0 1200 0 1200 120z' className='contact-divider-fill'></path>
+          <path d='M1200 120L0 16.48 0 0 1200 0 1200 120z' className='resume-divider-fill'></path>
         </svg>
       </div>
 
