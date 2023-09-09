d325205e7cc6574e09582608fa60d86f1a640961
ej8899 <ej8899@gmail.com>
2023-09-09 18:52:59 -0400

    add: context for modal windwos

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './index.scss';
-
+import { ModalManager } from './components/modal/ModalManager';
 /* 
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 */
+// TODO see above - turn strictmode back on
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <ModalManager>
+    <App />
+  </ModalManager>,
+);
