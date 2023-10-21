fb039df20706459d8b6839b2d9188979ff7e1165
ej8899 <ej8899@gmail.com>
2023-10-21 12:23:40 -0400

    update: update for TS compliance

--- src/components/SectionObserver.tsx
+++ src/components/SectionObserver.tsx
@@ -1,8 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable react/prop-types */
 import React from '../assets/components/React';
 import { useRef, useEffect, useState } from 'react';
 
