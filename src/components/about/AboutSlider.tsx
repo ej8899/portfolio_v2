1ca6c9b3b858b9eb80f06b582f8cd0f5d27c0b9f
ej <ej8899@gmail.com>
2023-10-27 12:02:58 -0400

    update: tweak misc styles

--- src/components/about/AboutSlider.tsx
+++ src/components/about/AboutSlider.tsx
@@ -26,6 +26,19 @@ function AboutSlider() {
 
   return (
     <section className='faqs' aria-labelledby='my skills and tools'>
+      <div className='custom-shape-divider-top-16940443142'>
+        <svg
+          data-name='Layer 1'
+          xmlns='http://www.w3.org/2000/svg'
+          viewBox='0 0 1200 120'
+          preserveAspectRatio='none'
+        >
+          <path
+            d='M1200 120L0 16.48 0 0 1200 0 1200 120z'
+            className='aboutskills-divider-fill'
+          ></path>
+        </svg>
+      </div>
       <div className='column centered_grid'>
         <h2 id='faqs' className='faqs__title'>
           Skills & Traits
