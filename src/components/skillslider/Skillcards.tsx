e0e3dc5ab7a8b174b9f69c8755ef4c0e0546f878
ej8899 <ej8899@gmail.com>
2023-10-29 19:13:31 -0400

    update: skill card content, tweak style, tweak animation

--- src/components/skillslider/Skillcards.tsx
+++ src/components/skillslider/Skillcards.tsx
@@ -3,11 +3,6 @@ import './SkillCards.scss';
 import { LegacyRef, useEffect, useRef } from 'react';
 import useElementOnScreen from '../../hooks/useElementOnScreen';
 
-/* good reference on sliders here:
-https://ryanmulligan.dev/blog/css-marquee/
-some of this was used below:
-*/
-
 const SkillCards = (props: { reference: LegacyRef<HTMLElement> | undefined }) => {
   const titleRef = useRef<HTMLHeadingElement>(null);
   const isOnScreen = useElementOnScreen(titleRef);
@@ -19,22 +14,33 @@ const SkillCards = (props: { reference: LegacyRef<HTMLElement> | undefined }) =>
 
   return (
     <div className='centered_container column centered_grid'>
+      <div className='column centered_grid studies_sticky'>
+        <br />
+        <h2 id='studies' className='studies__title'>
+          Current Studies
+        </h2>
+        <div className='subtitle-message'>Continuous learning is one reason why I love coding!</div>
+      </div>
       <div className='skill-details-container'>
         <div className='skill-details-column'>
           <ul id='skillcards'>
             <li className='skillcard' id='card_1'>
               <div className='skill-card__content skill-card'>
-                <h4>What is a Frontend Develoment?</h4>
+                <h4>MongoDB</h4>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam porro similique
-                  aliquid debitis ipsam soluta dolorum ipsa! Voluptate, suscipit iure.
+                  I&apos;ve been involved in SQL & Relational databases for many years, but at the
+                  same time always entertained by alternatives, however performance was always
+                  lacking. MongoDB seems to have largely solved the performance issues while giving
+                  developers scalability, simplicity and scalability.
+                  <br />
+                  I&apos;m eager to expand my knowledge of MongoDB.
                 </p>
               </div>
             </li>
 
             <li className='skillcard' id='card_2'>
               <div className='skill-card__content skill-card'>
-                <h4>What is a Frontend Develoment?</h4>
+                <h4>AWS</h4>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam porro similique
                   aliquid debitis ipsam soluta dolorum ipsa! Voluptate, suscipit iure.
@@ -44,10 +50,23 @@ const SkillCards = (props: { reference: LegacyRef<HTMLElement> | undefined }) =>
 
             <li className='skillcard' id='card_3'>
               <div className='skill-card__content skill-card'>
-                <h4>What is a Frontend Develoment?</h4>
+                <h4>Advanced Python</h4>
                 <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam porro similique
                   aliquid debitis ipsam soluta dolorum ipsa! Voluptate, suscipit iure.
+                </p>
+              </div>
+            </li>
+
+            <li className='skillcard' id='card_4'>
+              <div className='skill-card__content skill-card'>
+                <h4>AI Integration</h4>
+                <p>
+                  Although I utilize AI on a daily basis as a coding assistant and even as a general
+                  assistant, I have yet to incorporate AI into any applications on a significant
+                  <br />I have a few projects in the &apos;to do&apos; list as well as a couple of
+                  projects that I&apos;d like to add an AI componenent to them. Updates will be soon
+                  enough!
                 </p>
               </div>
             </li>
