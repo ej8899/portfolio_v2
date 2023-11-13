20441c47876a8866f0dfab12bd4daceb3b57ee19
ej <ej8899@gmail.com>
2023-11-13 11:56:06 -0500

    update: style and layout for blog posts

--- src/components/blog/Blogs.tsx
+++ src/components/blog/Blogs.tsx
@@ -1,6 +1,7 @@
 import React from '../../assets/components/React';
 import { LegacyRef, useEffect, useRef, useState } from 'react';
 import './Blogs.scss';
+import Collapse from '../collapse/Collapse';
 
 interface BlogPost {
   title: string;
@@ -8,6 +9,25 @@ interface BlogPost {
   keywords: string[];
   article: string;
 }
+
+const convertMarkdownToHtml = (markdown: string): string => {
+  if (!markdown) {
+    return '';
+  }
+
+  // Convert bold text: **text** to <strong>text</strong>
+  markdown = markdown.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
+  // Convert italic text: *text* to <em>text</em>
+  markdown = markdown.replace(/\*(.*?)\*/g, '<em>$1</em>');
+  // Convert images: ![alt text](url) to <img src="url" alt="alt text" />
+  markdown = markdown.replace(/!\[(.*?)\]\((.*?)\)/g, '<img src="$2" alt="$1" />');
+  // Convert horizontal rule: --- to <hr />
+  markdown = markdown.replace(/---/g, '<hr />');
+  // Convert links: [text](url) to <a href="url">text</a>
+  markdown = markdown.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
+
+  return markdown;
+};
 
 function BlogComponent() {
   const [blogPosts, setBlogPosts] = useState<{ [postId: string]: BlogPost }>({});
@@ -44,16 +64,20 @@ function BlogComponent() {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, react/prop-types, @typescript-eslint/no-unsafe-member-access
     <section id='blog' className='blog' aria-label='blog posts'>
       <div className='column full_height centered_grid'>
-        <h2 className='portfolio__title'>Blog Posts</h2>
-        <div className='portfolio__subtitle-message subtitle-message'>Some of my Ramblings</div>
+        <h2 className='blog__title'>Blog Posts</h2>
+        <div className='blog__subtitle-message subtitle-message'>Some of my Ramblings</div>
         <ul>
           {Object.entries(blogPosts).map(([postId, post]) => (
             <li key={postId}>
-              <h2>{post.title}</h2>
-              <p>Date: {post.date}</p>
-              <p>ID: {postId}</p>
-              <p>Keywords: {post.keywords.join(', ')}</p>
-              <p>Article: {post.article}</p>
+              <Collapse title={post.title} key={postId}>
+                <p>Date: {post.date}</p>
+                <p>ID: {postId}</p>
+                <p>Keywords: {post.keywords.join(', ')}</p>
+                <div
+                  className='project__text-p'
+                  dangerouslySetInnerHTML={{ __html: convertMarkdownToHtml(post.article) }}
+                ></div>
+              </Collapse>
             </li>
           ))}
         </ul>
